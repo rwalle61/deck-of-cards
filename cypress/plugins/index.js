5100326825f109e459b1030b8451c9808bb6040e
Richard Waller <Richard.Waller@ibm.com>
2020-03-08 18:18:08 +0000

    test: retry cypress tests on intermittent failure

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -8,10 +8,10 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 
+const cypressPluginRetries = require('cypress-plugin-retries/lib/plugin');
+
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+    cypressPluginRetries(on);
+};
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,3 +15,4 @@
 
 import './commands';
 
+require('cypress-plugin-retries');
