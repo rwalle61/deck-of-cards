9c6d64f45464783ce1878129932dac9ce1ded3e4
Richard Waller <Richard.Waller@ibm.com>
2020-03-09 12:46:50 +0000

    app can be deployed to ibm cloud

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,3 +1,21 @@
-const origin = 'http://localhost:9100';
+const port = process.env.PORT || 9100;
+
+// For debug
+// console.log('process.env');
+// console.log(process.env);
+// console.log('port', port);
+// console.log('process.env.REACT_APP_SERVER_LOCATION');
+// console.log(process.env.REACT_APP_SERVER_LOCATION);
+
+const origins = {
+  cloud: 'https://deck-of-cards.eu-gb.cf.appdomain.cloud',
+  local: `http://localhost:${port}`,
+};
+
+const serverLocation = process.env.REACT_APP_SERVER_LOCATION || 'local';
+
+const origin = origins[serverLocation];
+
+console.log(`Client expects server at origin: ${origin}`);
 
 export default origin;
