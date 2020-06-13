d2140466fa6c6ddba1e44e50c17c1bd894cbb075
Richard Waller <Richard.Waller@ibm.com>
2020-06-13 14:41:27 +0100

    test: use snapshotDiff in tests

--- client/src/components/Controls/Controls.test.js
+++ client/src/components/Controls/Controls.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
+import snapshotDiff from 'snapshot-diff';
 import Controls from '.';
 
 describe('<Controls />', () => {
@@ -9,63 +11,101 @@ describe('<Controls />', () => {
     );
     expect(wrapper).toHaveLength(1);
   });
-  it('contains 3 buttons', () => {
-    const wrapper = shallow(
+  it('contains 3 buttons, enabled by default', () => {
+    const screen = render(
       <Controls onShuffle={() => {}} onDraw={() => {}} onSort={() => {}} />,
     );
-    expect(wrapper.find('.btn')).toHaveLength(3);
-  });
-  it('matches the snapshot', () => {
-    const wrapper = shallow(
-      <Controls onShuffle={() => {}} onDraw={() => {}} onSort={() => {}} />,
-    );
-    expect(wrapper).toMatchInlineSnapshot(`
-      <Container
-        className="Controls"
-        fluid={false}
-      >
-        <Button
-          active={false}
-          className="btn Shuffle-btn"
-          disabled={false}
-          onClick={[Function]}
-          size="lg"
+    const buttons = screen.getAllByRole('button');
+    expect(buttons).toHaveLength(3);
+    expect(buttons).toMatchInlineSnapshot(`
+      Array [
+        <button
+          class="btn Shuffle-btn btn btn-outline-warning btn-lg"
           type="button"
-          variant="outline-warning"
         >
           Shuffle Deck
-        </Button>
-        <Button
-          active={false}
-          className="btn Draw-btn"
-          disabled={false}
-          onClick={[Function]}
-          size="lg"
+        </button>,
+        <button
+          class="btn Draw-btn btn btn-outline-success btn-lg"
           type="button"
-          variant="outline-success"
         >
           Draw Card
-        </Button>
-        <Button
-          active={false}
-          className="btn Sort-btn"
-          disabled={false}
-          onClick={[Function]}
-          size="lg"
+        </button>,
+        <button
+          class="btn Sort-btn btn btn-outline-primary btn-lg"
           type="button"
-          variant="outline-primary"
         >
           Sort Hand
-        </Button>
-      </Container>
+        </button>,
+      ]
     `);
   });
-  it('has a shuffle button that can be clicked', () => {
+  it('has a shuffle button, enabled by default', () => {
     const onShuffle = jest.fn();
     const wrapper = shallow(
       <Controls onShuffle={onShuffle} onDraw={() => {}} onSort={() => {}} />,
     );
     wrapper.find('.Shuffle-btn').simulate('click');
     expect(onShuffle).toHaveBeenCalled();
+  });
+  it('disables the draw and shuffle buttons when deckLength is 0', () => {
+    const screen1 = render(
+      <Controls onShuffle={() => {}} onDraw={() => {}} onSort={() => {}} />,
+    );
+    const screen2 = render(
+      <Controls
+        onShuffle={() => {}}
+        onDraw={() => {}}
+        onSort={() => {}}
+        deckLength={0}
+      />,
+    );
+    expect(
+      snapshotDiff(screen1.container, screen2.container, { contextLines: 2 }),
+    ).toMatchInlineSnapshot(`
+      "Snapshot Diff:
+      - First value
+      + Second value
+
+      @@ -5,4 +5,5 @@
+            <button
+              class=\\"btn Shuffle-btn btn btn-outline-warning btn-lg\\"
+      +       disabled=\\"\\"
+              type=\\"button\\"
+            >
+      @@ -11,4 +12,5 @@
+            <button
+              class=\\"btn Draw-btn btn btn-outline-success btn-lg\\"
+      +       disabled=\\"\\"
+              type=\\"button\\"
+            >"
+    `);
+  });
+  it('disables the sort button when handLength is 0', () => {
+    const screen1 = render(
+      <Controls onShuffle={() => {}} onDraw={() => {}} onSort={() => {}} />,
+    );
+    const screen2 = render(
+      <Controls
+        onShuffle={() => {}}
+        onDraw={() => {}}
+        onSort={() => {}}
+        handLength={0}
+      />,
+    );
+    expect(
+      snapshotDiff(screen1.container, screen2.container, { contextLines: 2 }),
+    ).toMatchInlineSnapshot(`
+      "Snapshot Diff:
+      - First value
+      + Second value
+
+      @@ -17,4 +17,5 @@
+            <button
+              class=\\"btn Sort-btn btn btn-outline-primary btn-lg\\"
+      +       disabled=\\"\\"
+              type=\\"button\\"
+            >"
+    `);
   });
 });
